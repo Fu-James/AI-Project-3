Modify belief_state calculation and fix examine()     - Agent6.update_belief_state()         Change to the correct calculation         Instead of updating the whole belief to make them sum to 1, just update the belief based on the relative probability     - Fix Agent6.examine()         Problem: Examine always gives True if target exist         Status: fixed

agent_6.py
@@ -25,8 +25,16 @@ class Agent6:
         self._belief_state = np.array([[(1 / (self._dim ** 2)) for _ in range(self._dim)] 
                               for _ in range(self._dim)])
     
-    def update_belief_state(self, scalar: float) -> None:
-        self._belief_state *= scalar
+    def update_belief_state(self, cell: Cell, status: Status) -> None:
+        if status == Status.Blocked:
+            self._belief_state[cell.x, cell.y] = 0
+        elif status == Status.Empty:
+            pass
+        elif status == Status.Examine:
+            fnr = self._fnr[cell.get_terrain_type().value]
+            self._belief_state[cell.x, cell.y] *= (1 - fnr)
+        else:
+            raise ValueError('Status for calling get_scalar should be either blocked, empty, or examine')
 
     def get_neighbors(self, pos: tuple) -> list[tuple]:
         neighbors = []
@@ -78,10 +86,12 @@ class Agent6:
             raise Exception('Logical error in breadth_first_search()')
         return list(choice(targets))
         
-    def examine(self, cell: Cell, terrain_type: TerrainType) -> bool:
-        return bool(self._maze.get_cell(cell.x, cell.y).is_target() * self._fnr[terrain_type.value])
+    def examine(self, cell: Cell) -> bool:
+        terrain_type = cell.get_terrain_type()
+        return uniform(0, 1) > self._fnr[terrain_type.value] if self._maze.get_cell(cell.x, cell.y).is_target() else False
 
     def execute(self, path: list[Cell]) -> list:
+        cell_in_knowledge = None
         for count, cell in enumerate(path):
             cell_in_maze = self._maze.get_cell(cell.x, cell.y)
             cell_in_knowledge = self._knowledge.get_cell(cell.x, cell.y)
@@ -89,27 +99,28 @@ class Agent6:
                 cell_in_knowledge.isVisited = True
                 if cell_in_maze.is_blocked():
                     cell_in_knowledge.set_status(Status.Blocked)
-                    self.update_belief_state(self.get_scalar(self._belief_state[cell.x][cell.y], 
-                                                            Status.Blocked))
-                    self._belief_state[cell.x, cell.y] = 0
+                    self.update_belief_state(cell_in_knowledge, Status.Blocked)
                     return cell.get_parent(), count - 1, 'blocked'
                 else:
                     cell_in_knowledge.set_status(Status.Empty)
+                    cell_in_knowledge.set_terrain_type(cell_in_maze.get_terrain_type())
+                    self.update_belief_state(cell_in_knowledge, Status.Empty)
 
-        cell = path[-1]
-        terrain_type = self._maze.get_cell(cell.x, cell.y).get_terrain_type()
-        if self.examine(cell, terrain_type):
-            return cell, count, 'find goal'
+        if cell_in_knowledge is None:
+            raise Exception('Error!')
+        if self.examine(cell_in_knowledge):
+            return cell_in_knowledge, count, 'find goal'
         else:
-            belief = self._belief_state[cell.x][cell.y]
-            self.update_belief_state(self.get_scalar(belief, Status.Examine, terrain_type))
-            self._belief_state[cell.x, cell.y] = belief * self._fnr[terrain_type.value]
-            return cell, count, 'examine failed'
+            self.update_belief_state(cell_in_knowledge, Status.Examine)
+            return cell_in_knowledge, count, 'examine failed'
 
     def solve(self, start: list, steps: int) -> list:
         examine_count = 0
         total_path = []
         time_to_find_goal = 0
+        time_to_run_astar = 0
+        astar_run_count = 0
+
         for _ in range(steps):
             goal = []
             path = []
@@ -118,14 +129,20 @@ class Agent6:
                 goal = self.get_target(start)
                 end_time = time.time_ns()
                 time_to_find_goal += end_time - start_time
-                path = astar_search(self._knowledge.get_cell(start[0], start[1]), goal, self._knowledge)
+
+                start_time = time.time_ns()
+                path = astar_search(start, goal, self._knowledge)
+                end_time = time.time_ns()
+                time_to_run_astar += end_time - start_time
+                astar_run_count += 1
+
                 if path != None:
                     break
-                # Not sure if we are allow to do this
-                self.update_belief_state(self.get_scalar(self._belief_state[goal[0]][goal[1]], 
-                                                         Status.Blocked))
-                self._belief_state[goal[0], goal[1]] = 0
-                self._knowledge.get_cell(goal[0], goal[1]).set_status(Status.Blocked)
+
+                # If the cell is unreachable, we can assume the cell is blocked
+                cell_in_knowledge = self._knowledge.get_cell(goal[0], goal[1])
+                cell_in_knowledge.set_status(Status.Blocked)
+                self.update_belief_state(cell_in_knowledge, Status.Blocked)
 
             end_cell, count, status_string = self.execute(path)
 
@@ -135,7 +152,9 @@ class Agent6:
                 examine_count += 1
 
             if status_string == 'find goal':
-                print(f'time to find the goal = {time_to_find_goal}')
+                print(f'A* search run {astar_run_count} times')
+                print(f'time to run A* search: {time_to_run_astar}')
+                print(f'time to guess goals: {time_to_find_goal}')
                 return examine_count, total_path, status_string
         
         return examine_count, total_path, 'cannot find goal in limited steps'